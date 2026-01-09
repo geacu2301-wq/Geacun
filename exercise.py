97a794b01e895fdddf07d48addefe1865ce48963
Dang Nguyen Xuan Quang <xuanweii@MacBookAir.lan>
2026-01-09 12:07:59 +0700

    Update exercise: ask name and year

---
+++ exercise.py
@@ -0,0 +1,9 @@
+name = input (" Please type in name ") 
+year = input (" Please type in year ")
+print(
+    f"{name} is a valiant knight, born in the year {year}. "
+    f"One morning, {name} woke up to an awful racket: a dragon "
+    f"was approaching the village. Only {name} was brave enough "
+    f"to protect the village."
+)  
+
