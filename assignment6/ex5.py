76c8148f08ccaf9ee3acf432e9000ec9dd36002c
Dang Nguyen Xuan Quang <xuanweii@MacBookAir.lan>
2026-03-19 09:56:37 +0700

    update code

--- assignment6/ex5.py
+++ assignment6/ex5.py
@@ -1,10 +1,11 @@
-def remove_odd(numbers):
+def remove_old(numbers):
     new_numbers = []
     for num in numbers:
         if num % 2 == 0:
             new_numbers.append(num)
     return new_numbers
 numbers = [23,24,25,26,27,28,29,30]
-result = remove_odd(numbers)
+result = remove_old(numbers)
 print("Original list:", numbers)
-print("The new list after removing odd numbers:", result)
+print("The new list after removing old numbers:", result)
+
