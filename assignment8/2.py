5046afd13d1566284a098e6e315852024bac1e87
Dang Nguyen Xuan Quang <xuanweii@Dangs-MacBook-Air.local>
2026-04-02 16:40:58 +0700

    update code

--- assignment8/2.py
+++ assignment8/2.py
@@ -3,13 +3,12 @@ class Building:
         self.bottom = middle
         self.top = top
         self.elevators = []
-
         for i in range(num_elevators):
             self.elevators.append(Elevator(middle, top))
     def run_elevator(self, elevators_number, destination):
         print(f"\nRunning elevator {elevators_number} to floor {destination}")
         self.elevators[elevators_number].go_to_floor(destination)
     def fire_alarm(self):
-        print("\n FIRE ALARM ACTIVATED! All elevators go to bottom of floor!")
+        print("\n FIRE ALARM ACTIVATED! ALL ELEVATORS GO TO THE PEAK OF BOTTOM!")
         for elevator in self.elevators:
             elevator.go_to_floor(self.bottom)
