aa9cc39b7d854e86b822628ddc1fef53fab23452
Dang Nguyen Xuan Quang <xuanweii@Dangs-MacBook-Air.local>
2026-04-02 15:41:55 +0700

    update code

--- assignment8/3.py
+++ assignment8/3.py
@@ -1,51 +1,29 @@
-
-import random
-class Car:
-    def __init__(self, reg_number, highest_speed):
-        self.reg_number = reg_number
-        self.max_speed = highest_speed   
-        self.speed = 0
-        self.distance = 0
-    def accelerate(self, speed_decrease_increase):
-        self.speed += speed_decrease_increase
-        if self.speed > self.max_speed:  
-            self.speed = self.max_speed
-        if self.speed < 0:
-            self.speed = 0
-    def drive(self, hours):
-        self.distance += self.speed * hours
-class Race:
-    def __init__(self, name, distance, cars):
-        self.name = name
-        self.distance = distance
-        self.cars = cars
-    def hour_passes(self):
-        for car in self.cars:
-            change = random.randint(-10, 15)
-            car.accelerate(change)
-            car.drive(1)
-    def print_status(self):
-        print("\nRACE STATUS")
-        print(f"{'Car':<10}{'Speed(km/h)':<15}{'Distance(km)':<15}")
-        for car in self.cars:
-            print(f"{car.reg_number:<10}{car.speed:<15}{car.distance:<15.1f}")
-    def race_finished(self):
-        for car in self.cars:
-            if car.distance >= self.distance:
-                return True
-        return False
-cars = []
-for i in range(10):
-    reg = f"ABC-{i+1}"
-    max_speed = random.randint(150, 200)
-    cars.append(Car(reg, max_speed))
-race = Race("Grand Demolition Derby", 8000, cars)
-hours = 0
-while not race.race_finished():
-    race.hour_passes()
-    hours += 1
-    if hours % 10 == 0:
-        print(f"\nHour: {hours}")
-        race.print_status()
-print("\n the end of reuslut:")
-race.print_status()
+class Elevator:
+    def __init__(self, peak, top):
+        self.bottom = peak
+        self.top = top
+        self.current_floor =peak
+    def floor_up(self):
+        if self.current_floor < self.top:
+            self.current_floor += 1
+            print(f"Elevator at floor {self.current_floor}")
+    def floor_down(self):
+        if self.current_floor > self.peak:
+            self.current_floor -= 1
+            print(f"Elevator at floor {self.current_floor}")
+    def go_to_floor(self, target_floor):
+        while self.current_floor < target_floor:
+            self.floor_up()
+        while self.current_floor > target_floor:
+            self.floor_down()
+class Building:
+    def __init__(self, peak, top, number_of_elevators):
+        self.elevators = []
+        for i in range(number_of_elevators):
+            self.elevators.append(Elevator(peak, top))
+    def run_elevator(self, elevator_number, target_floor):
+        self.elevators[elevator_number].go_to_floor(target_floor)
+    def fire_alarm(self):
+        print("\n  ALARM ACTIVATED ")
+        for elevator in self.elevators:
+            elevator.go_to_floor(elevator.peak)
