51f66c3a7cd52f15094184c15bbc2e537148abfc
Dang Nguyen Xuan Quang <xuanweii@Dangs-MacBook-Air.local>
2026-04-03 08:44:22 +0700

    update code

--- assignment9/bai4.py
+++ assignment9/bai4.py
@@ -1,1 +1,23 @@
-print ("hello world")
+def caesar_cipher(bai4, shift, direction):
+    with open(bai4, 'r') as file:
+        text = file.read()
+    result = ""
+    for c in text:
+        if 'A' <= c <= 'Z':
+            if direction == "right":
+                new_char = chr((ord(c) - ord('A') + shift) % 26 + ord('A'))
+            else:
+                new_char = chr((ord(c) - ord('A') - shift) % 26 + ord('A'))
+        elif 'a' <= c <= 'z':
+            if direction == "right":
+                new_char = chr((ord(c) - ord('a') + shift) % 26 + ord('a'))
+            else:
+                new_char = chr((ord(c) - ord('a') - shift) % 26 + ord('a'))
+        elif '0' <= c <= '9':  
+            new_char = c
+        else:
+            new_char = c
+        result += new_char
+    with open("ciphertext.txt", 'w') as file:
+        file.write(result)
+    return result
